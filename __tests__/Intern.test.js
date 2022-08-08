6af9dc38043c3f712043c415c62cb020221216f6
Erikson <chris.s.erickson@gmail.com>
2022-08-07 21:26:19 -0400

    added intern object with test

--- __tests__/Intern.test.js
+++ __tests__/Intern.test.js
@@ -1,7 +1,30 @@
+const Intern = require('../lib/Intern')
 // test for school
 
+const intern1 = new Intern ('mel', 5, 'mel@mel', 'Intern', 'TIU')
 
-// test for getSchool
+// test for name
+test ('has a name', () => {
+    expect(intern1.name).toEqual(expect.any(String))
+    expect(intern1.name.length).toBeGreaterThan(2)
+})
+// test for email
+test ('has an email', () =>{
+    expect(intern1.email).toEqual(expect.stringContaining('@'))
+})
+// test for id
+test ('has an id number', () => {
+    expect(intern1.id).toEqual(expect.any(Number))
+})
+// test for role
+test('has a role of Intern', () => {
+    expect(intern1.role).toBe('Intern')
+})
+
+test ('has selected a school', () => {
+    expect(intern1.school).toEqual(expect.any(String));
+})
+
 
 
 // test for getRole -- overridden to return 'Intern'
