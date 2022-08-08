450df21f68fcf988191ec12134c74e5849cd309e
Erikson <chris.s.erickson@gmail.com>
2022-08-07 21:39:47 -0400

    added manager object with tests

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -4,22 +4,22 @@ const Engineer = require('../lib/Engineer');
 const engineer1= new Engineer ('chris', 2, 'c@c', 'Engineer', 'ericksonvik')
 
 // test for name
-// test ('has a name', () => {
-//     expect(engineer1.name).toEqual(expect.any(String))
-//     expect(engineer1.name.length).toBeGreaterThan(2)
-// })
-// // test for email
-// test ('has an email', () =>{
-//     expect(engineer1.email).toEqual(expect.stringContaining('@'))
-// })
-// // test for id
-// test ('has an id number', () => {
-//     expect(engineer1.id).toEqual(expect.any(Number))
-// })
-// // test for role
-// test('has a role of engineer', () => {
-//     expect(engineer1.role).toBe('Engineer')
-// })
+test ('has a name', () => {
+    expect(engineer1.name).toEqual(expect.any(String))
+    expect(engineer1.name.length).toBeGreaterThan(2)
+})
+// test for email
+test ('has an email', () =>{
+    expect(engineer1.email).toEqual(expect.stringContaining('@'))
+})
+// test for id
+test ('has an id number', () => {
+    expect(engineer1.id).toEqual(expect.any(Number))
+})
+// test for role
+test('has a role of engineer', () => {
+    expect(engineer1.role).toBe('Engineer')
+})
 // test for github
 test ('has a github', () => {
     expect(engineer1.github).toEqual(expect.any(String));
--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,6 +1,29 @@
+const Manager = require('../lib/Manager')
 
+const manager1 = new Manager ('name', 3, 'man@mail', 'Manager', 10)
 
-//  test for officNumber
+// test for name
+test ('has a name', () => {
+    expect(manager1.name).toEqual(expect.any(String))
+    expect(manager1.name.length).toBeGreaterThan(2)
+})
+// test for email
+test ('has an email', () =>{
+    expect(manager1.email).toEqual(expect.stringContaining('@'))
+})
+// test for id
+test ('has an id number', () => {
+    expect(manager1.id).toEqual(expect.any(Number))
+})
+// test for role
+test('has a role of Manager', () => {
+    expect(manager1.role).toBe('Manager')
+})
+// test for office number
+test ('has an office number', () => {
+    expect(manager1.office).toEqual(expect.any(Number));
+})
+
 
 
 // test for getRole -- Overridden to retun 'Manager'
