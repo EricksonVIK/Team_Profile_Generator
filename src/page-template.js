8759b06a6cd01ae7c2dd4c5441ad48bf2ff28deb
Erikson <chris.s.erickson@gmail.com>
2022-08-09 19:33:36 -0400

    adding style

--- src/page-template.js
+++ src/page-template.js
@@ -5,15 +5,18 @@ const generateHtml = (teamData) => {
         const managerArr =[]
         for (let i =0; i < manager.length; i++){
         managerArr.push(
-        `<div class="flex-column justify-space-between align-center" id="employeeBlocks>
-                <h1 class="card-title" id="card-title">${manager[i].name}</h1>
+        `<div class="flex-column justify-space-between align-center" id="employeeBlocks">
+            <div class="card">
+                <h1 class="card-title">${manager[i].name}</h1>
                 <h3 class ="card-subtitle">${manager[i].role} </h3>
                 <ul class="list-group">
-                    <li class = "list-group-item"> Email: ${manager[i].email}</li>
+                    <li class = "list-group-item"> Email: <a href=mailto:${manager[i].email}>${manager[i].email}</a></li>
                     <li class = "list-group-item"> Employee ID: ${manager[i].id}</li>
                     <li class = "list-group-item"> Office #: ${manager[i].office}</li>
                 </ul>
-        </div>`)
+            </div>
+        </div>
+        `)
         }
         return managerArr.join('')
     }
@@ -44,6 +47,7 @@ const generateHtml = (teamData) => {
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Team Member Profile</title>
         <link href="https://fonts.googleapis.com/css?family=IBM+Plex+Sans:400,400i,700&display=swap" rel="stylesheet">
+        <link href= "https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css">      
         <link rel="stylesheet" href= "style.css">
         <link rel="stylesheet" href= "generalstyle.css">
@@ -54,10 +58,10 @@ const generateHtml = (teamData) => {
             MY TEAM
             </h1>
         </header>
-        <main class="flex-row justify-space-between">
-                    ${generateManager(teamData.filter(data => data.role === 'Manager'))}
-                    ${generateEngineer(teamData.filter(data => data.role === 'Engineer'))}
-                    ${generateIntern(teamData.filter(data => data.role === 'Intern'))}
+        <main class="flex-row col-12">
+            ${generateManager(teamData.filter(data => data.role === 'Manager'))}
+            ${generateEngineer(teamData.filter(data => data.role === 'Engineer'))}
+            ${generateIntern(teamData.filter(data => data.role === 'Intern'))}
         </main>
 
     </body>
